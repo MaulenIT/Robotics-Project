cce5fdef07475eaeac8ca78285e0dd32b4ddf57e
Maulen <213009edu@gmail.com>
2024-02-23 01:51:15 +0600

    The code that I proposed simulates the movement of a differential-drive robot using the formulas from task 2

---
+++ task2.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameters for simulation
+dt = 0.1
+total_time = 9.78
+v = 0.30
+omega = 0.0
+
+x = np.array([0.0, 0.0, 0.0])
+
+# Store history for plotting
+history = np.zeros((int(total_time / dt) + 1, 3))
+history[0] = x
+
+for i in range(1, int(total_time / dt) + 1):
+    dx = np.array([np.cos(x[2]), np.sin(x[2]), omega])
+
+    x += v * dx * dt
+
+    history[i] = x
+
+x_hist, y_hist, theta_hist = history[:, 0], history[:, 1], history[:, 2]
+
+plt.figure()
+plt.plot(x_hist, y_hist, label='Robot Path')
+plt.quiver(x_hist[::10], y_hist[::10], np.cos(theta_hist[::10]), np.sin(theta_hist[::10]), scale=20)
+plt.xlabel('X position')
+plt.ylabel('Y position')
+plt.title('Simulation of Dead Reckoning Localization')
+plt.axis('equal')
+plt.grid(True)
+plt.legend()
+plt.show()
