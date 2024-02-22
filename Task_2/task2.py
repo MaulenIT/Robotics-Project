3352212c980ed4cfc9106c890f41345c010a8dc6
Maulen <213009edu@gmail.com>
2024-02-23 02:11:38 +0600

    # Add Gaussian noise to the velocities

--- Task_2/task2.py
+++ Task_2/task2.py
@@ -6,7 +6,9 @@ dt = 0.1
 total_time = 9.78
 v = 0.30
 omega = 0.0
-
+mu = 0
+sigma_v = 0.01
+sigma_omega = 0.005
 x = np.array([0.0, 0.0, 0.0])
 
 # Store history for plotting
@@ -14,6 +16,9 @@ history = np.zeros((int(total_time / dt) + 1, 3))
 history[0] = x
 
 for i in range(1, int(total_time / dt) + 1):
+    v_noisy = v + np.random.normal(mu, sigma_v)
+    omega_noisy = omega + np.random.normal(mu, sigma_omega)
+
     dx = np.array([np.cos(x[2]), np.sin(x[2]), omega])
 
     x += v * dx * dt
